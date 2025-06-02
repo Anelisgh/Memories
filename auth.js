05ae628eebbeaccd5b90a1c89bb5371d7364ac79
anelisgh <anelis.gheorghias94@gmail.com>
2025-06-02 13:25:45 +0300

    login

---
+++ auth.js
@@ -0,0 +1,21 @@
+async function checkPassword() { 
+  const passwordInput = document.getElementById('password').value;
+  const errorElement = document.getElementById('error-message');
+  
+  const storedHash = "297581d6cd198a6e6df740f13288cb13a1e76cebe3f0ebc3fe259977addfd646"; 
+  const inputHash = await sha256(passwordInput);
+  
+  if (inputHash === storedHash) {
+    sessionStorage.setItem('authenticated', 'true');
+    window.location.href = 'site.html';
+  } else {
+    errorElement.textContent = 'Parolă incorectă!';
+  }
+}
+
+async function sha256(message) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
